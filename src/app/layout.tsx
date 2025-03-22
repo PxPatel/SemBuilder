aa887ed894ee5fef5d6fca48cfa550e849d9c053
Priyansh Patel <youname972@gmail.com>
2025-03-22 17:51:33 -0400

    fix: Analytic component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
           <SchedulerProvider>{children}</SchedulerProvider>
         </ErrorUIProvider>
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
